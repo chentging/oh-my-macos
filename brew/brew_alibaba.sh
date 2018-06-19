8cc17f5ad898a35f0459a6e87ed38fbc3bab8a65
rainplus <rainplus@foxmail.com>
2018-06-19 23:17:57 +0800

    添加 opsx 的 homebrew 配置

---
+++ brew/brew_alibaba.sh
@@ -0,0 +1,10 @@
+cd "$(brew --repo)" 
+git remote set-url origin https://mirrors.aliyun.com/homebrew/brew.git
+
+cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core" 
+git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git 
+
+echo "export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles" >>$HOME/.bashrc
+
+
+brew update 
